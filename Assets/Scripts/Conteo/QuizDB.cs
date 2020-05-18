140cbd9ba8fe5ccde96a396d62d42e9c23d18ed0
LichKiller <adonis.villalva@outlook.com>
2020-05-18 01:00:43 -0500

    Initial_Conteo
    Inicil para el minijuego de Conteo - Desarrollo Scripts V 0.1

---
+++ Assets/Scripts/Conteo/QuizDB.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizDB : MonoBehaviour
+{
+    [SerializeField] private List<Question> questionList = null;
+}
